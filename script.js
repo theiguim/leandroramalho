e24b776df8aab0108be62f45c7104bf905905c9f
theiguim <theiguim@gmail.com>
2025-02-27 16:05:45 -0300

    finish pag

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
             clipPath: "inset(0% 100% 0% 0%)",
             scrollTrigger: {
                 trigger: ".main-img",
-                start: "top top",
+                start: "center",
                 end: "bottom top",
                 scrub: true,
             }
@@ -20,13 +20,25 @@ document.addEventListener("DOMContentLoaded", function () {
             x: 420, opacity: 0,
             scrollTrigger: {
                 trigger: ".main-txt",
-                start: "top top",
+                start: "center",
                 end: "bottom top",
                 scrub: true,
             }
         }
     );
 
+    gsap.from(".gallery", {
+        x: "-100%", // Vem da esquerda
+        opacity: 0, // Começa invisível
+        duration: 1.2, // Tempo da animação
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: ".gallery",
+          start: "top 40%", // Inicia quando 80% da tela atinge o topo da section
+          toggleActions: "play reverse play reverse", // Reanima ao entrar e reverte ao sair
+        }
+      });
+    
     gsap.set(".about-txt h1", { opacity: 0, x: -100 });
     gsap.set(".line", { scaleX: 0, transformOrigin: "left center" });
     gsap.set(".about-txt p", { opacity: 0, y: 30 });
@@ -43,6 +55,55 @@ document.addEventListener("DOMContentLoaded", function () {
         .to(".about-txt p", { opacity: 1, y: 0, duration: 0.8, stagger: 0.3 }, "-=0.3");
 
 
+
+
+    const galleryContainer = document.querySelector('.gallery-container')
+    const galleryItems = document.querySelectorAll(".gallery-item")
+
+    // Seleciona os botões criados manualmente
+    const prevButton = document.getElementById("prevButton")
+    const nextButton = document.getElementById("nextButton")
+
+    class Carousel {
+        constructor(container, items) {
+            this.carouselContainer = container
+            this.carouselArray = [...items]
+        }
+
+        updateGallery() {
+            this.carouselArray.forEach(el => {
+                el.classList.remove('gallery-item-1', 'gallery-item-2', 'gallery-item-3', 'gallery-item-4', 'gallery-item-5')
+            })
+
+            this.carouselArray.slice(0, 5).forEach((el, i) => {
+                el.classList.add(`gallery-item-${i + 1}`)
+            })
+        }
+
+        setCurrentState(direction) {
+            if (direction === 'prev') {
+                this.carouselArray.unshift(this.carouselArray.pop())
+            } else {
+                this.carouselArray.push(this.carouselArray.shift())
+            }
+            this.updateGallery()
+        }
+    }
+
+    // Inicializa o carrossel
+    const carousel = new Carousel(galleryContainer, galleryItems)
+
+    // Adiciona eventos de clique nos botões
+    prevButton.addEventListener("click", () => {
+        carousel.setCurrentState("prev")
+    })
+
+    nextButton.addEventListener("click", () => {
+        carousel.setCurrentState("next")
+    })
+
+
+
     const form = document.getElementById("my-form");
 
     form.addEventListener("submit", async (e) => {
@@ -50,7 +111,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const formData = new FormData(e.target);
         const data = Object.fromEntries(formData.entries());
-        
+
         const name = data.FNAME
         const phone = data.PHONE
 
@@ -58,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const response = await fetch("http://localhost:3000/webhook", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({name, phone}),
+                body: JSON.stringify({ name, phone }),
             });
 
             const data = await response.json();
